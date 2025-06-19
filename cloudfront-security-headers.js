09d28e3600771882c063e8a990490c29f5c94222
Alejandro Gil <alejandro@alejandrogil.net>
2025-06-18 21:31:13 -0700

    Fix CSP connect-src to allow Lambda Function URLs - Add all 6 Lambda Function URLs to Content-Security-Policy connect-src directive - Resolves: Content-Security-Policy blocked resource at lambda-url.us-east-1.on.aws - Allows subscription check and payment API calls to work properly

--- cloudfront-security-headers.js
+++ cloudfront-security-headers.js
@@ -10,7 +10,15 @@ function handler(event) {
             "style-src 'self' 'unsafe-inline' fonts.googleapis.com; " +
             "font-src 'self' fonts.gstatic.com; " +
             "img-src 'self' data: blob: https:; " +
-            "connect-src 'self' wss: https: blob:; " +
+            "connect-src 'self' " +
+            "wss://relay.damus.io wss://nos.lol wss://relay.primal.net wss://relay.nostr.band " +
+            "https://si4pgos4rx3nykzya2y5ugcp6a0vwgdj.lambda-url.us-east-1.on.aws/ " +
+            "https://r7sts256fb7qrwouximz3x2q6y0yroyx.lambda-url.us-east-1.on.aws/ " +
+            "https://trfdrrplkdsjo3kynxuprpfnre0pxklc.lambda-url.us-east-1.on.aws/ " +
+            "https://ynbxffgyxiqun476l2oa27qyma0neved.lambda-url.us-east-1.on.aws/ " +
+            "https://w3ttcgeoojib2wi3ymbzafekfa0cdfmc.lambda-url.us-east-1.on.aws/ " +
+            "https://tkrxeipv25uhrl7vea7h2rijgy0duzvo.lambda-url.us-east-1.on.aws/ " +
+            "wss: https: blob:; " +
             "frame-src 'self'; " +
             "object-src 'none'; " +
             "base-uri 'self';"
