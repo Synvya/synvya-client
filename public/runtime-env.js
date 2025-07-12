553c185041013717de6e00d1285bc7a0b114f041
Alejandro Gil <alejandro@alejandrogil.net>
2025-07-11 22:46:50 -0700

    Fix script permissions and add real Lambda URLs

---
+++ public/runtime-env.js
@@ -0,0 +1,5 @@
+window.runtimeEnv = {
+  healthUrl: 'https://vttisrqb56t55j3svc3yuw7glq0kcxfq.lambda-url.us-east-1.on.aws/',
+  recordTermsAcceptanceUrl: 'https://emje6a52qf6k7iuxdi4hkttpn40rxjjz.lambda-url.us-east-1.on.aws/',
+  checkUserExistsUrl: 'https://fogdgbayg74hgubolh7oc7rwyi0rkaep.lambda-url.us-east-1.on.aws/'
+};
